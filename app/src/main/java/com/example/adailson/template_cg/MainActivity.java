3ad020096d5735527e3d580a0e6d45cdb5fab193
Adailsonacj <adailsonacj@live.com>
2018-09-26 23:19:28 -0300

    Implementado interação com acelerômetro.

--- app/src/main/java/com/example/adailson/template_cg/MainActivity.java
+++ app/src/main/java/com/example/adailson/template_cg/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.adailson.template_cg;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //Variavel de referencia para o objeto de desenho
     GLSurfaceView superficieDesenho = null;
     Renderizador render = null;
+    public static float sensorDirecaoX;
+    public static float sensorDirecaoY;
+
+    private Sensor accelerometer;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +39,54 @@ public class MainActivity extends AppCompatActivity {
 
         //Publica a sup de desenho na tela do app
         setContentView(superficieDesenho);
+
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, accelerometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+
+        sensorDirecaoX = sensorEvent.values[0];
+        sensorDirecaoY = sensorEvent.values[1];
+
+//        if (x < 0) {
+//            Log.i("SENSOR", "DIREITA");
+//            sensorDirecaoX = 100;
+//
+//        }
+//        if (x > 0) {
+//            Log.i("SENSOR", "ESQUERDA");
+//            sensorDirecaoX = -100;
+//        }
+//        if (y < 0) {
+//            Log.i("SENSOR", "frente baixa");
+//        }
+//        if (y > 0) {
+//            Log.i("SENSOR", "frente alta");
+//        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
--- app/src/main/java/com/example/adailson/template_cg/Renderizador.java
+++ app/src/main/java/com/example/adailson/template_cg/Renderizador.java
@@ -1,5 +1,8 @@
 package com.example.adailson.template_cg;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.opengl.GLSurfaceView;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -16,7 +19,7 @@ import javax.microedition.khronos.opengles.GL10;
 //essa classe implementa os métodos necessários para
 //utilizar a biblioteca OPENGL no desenho gráfico que
 // sera apresentado na tela pela superficie de desenho
-class Renderizador implements GLSurfaceView.Renderer , View.OnTouchListener {
+class Renderizador implements GLSurfaceView.Renderer, View.OnTouchListener {
     int fps;
     long inicial = 0;
     long atual = 0;
@@ -29,7 +32,6 @@ class Renderizador implements GLSurfaceView.Renderer , View.OnTouchListener {
     int alturaY = 0;
     int larguraX = 0;
     float angulo = 0;
-
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -106,6 +108,33 @@ class Renderizador implements GLSurfaceView.Renderer , View.OnTouchListener {
         //Este método é chamado n vezes por segundo para desenhar na tela.
         //(Determina o FPS)
         //Solicita openGL permissao para usar vetores de vertices
+//        posX = MainActivity.sensorDirecaoX;
+        if (posX >= larguraX) {
+            posX = larguraX;
+        }
+        if (posX <= 0) {
+            posX = 0;
+        }
+        if (posY >= alturaY) {
+            posY = alturaY;
+        }
+        if (posY <= 0) {
+            posY = 0;
+        }
+        if (MainActivity.sensorDirecaoX < 3) {
+            posX += 10;
+        }
+        if (MainActivity.sensorDirecaoX > -3) {
+            posX += -10;
+        }
+        if (MainActivity.sensorDirecaoY < 3) {
+            posY += 10;
+        }
+        if (MainActivity.sensorDirecaoY > -3) {
+            posY += -10;
+        }
+
+        Log.i("SENSOR", "" + posX + "");
 //        posX += direcaox * 5;
 //
 //        if (posX + 50 / 2 >= larguraX || posX <= 0) {
@@ -122,16 +151,15 @@ class Renderizador implements GLSurfaceView.Renderer , View.OnTouchListener {
 
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         gl.glLoadIdentity();
-        gl.glTranslatef(direcaox, direcaoY, 0);
-        gl.glRotatef(angulo,0,0,1);
+        gl.glTranslatef(posX, posY, 0);
+        gl.glRotatef(angulo, 0, 0, 1);
 
         gl.glColor4f(0, 0, 1, 0);
         gl.glVertexPointer(2, GL10.GL_FLOAT, 0, buffer);
         gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
 
 
-//        gl.glLoadIdentity();
-//        gl.glTranslatef(posX, posY, 0);
+        gl.glLoadIdentity();
 //        gl.glRotatef(angulo, 0, 0, 1);
         angulo += 5;
 
@@ -166,10 +194,10 @@ class Renderizador implements GLSurfaceView.Renderer , View.OnTouchListener {
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-            direcaox = (int) motionEvent.getX();
-            direcaoY = alturaY - (int) motionEvent.getY();
-        }else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+        if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+            posX = (int) motionEvent.getX();
+            posY = alturaY - (int) motionEvent.getY();
+        } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
 
         }
         return true;
